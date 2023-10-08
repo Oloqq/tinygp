6df1746b56315cbe621e8244d5361d77d71e0ec1
Olgierd Piofczyk <olgierdpk@gmail.com>
2023-10-08 23:27:02 +0200

    from problem

--- tiny_gp/src/main.rs
+++ tiny_gp/src/main.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code, unused_variables)]
 
 use std::error::Error;
-// use std::fs;
+use std::fmt::Display;
+use std::fs;
 
 type Berror = Box<dyn Error>;
 type Case = Vec<f32>;
@@ -14,47 +15,78 @@ struct Params {
     random_number: i32, // what is this
 }
 
-struct TinyGP {}
+const MAX_LEN: usize = 10000;
+const POPSIZE: usize = 100000;
+const DEPTH: usize = 5;
+const CROSSOVER_PROB: f32 = 0.9;
+const PMUT_PER_NODE: f32 = 0.05;
+const TOURNAMENT_SIZE: usize = 2;
+
+impl Display for Params {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let seed = self.seed;
+        let snr = self.min_random;
+        let sxr = self.max_random;
+        f.write_str(format!("SEED={seed}\nMAX_LEN={MAX_LEN})
+POPSIZE={POPSIZE}
+DEPTH={DEPTH})
+CROSSOVER_PROB={CROSSOVER_PROB})
+PMUT_PER_NODE={PMUT_PER_NODE})
+MIN_RANDOM={snr})
+MAX_RANDOM={sxr})
+TSIZE={TOURNAMENT_SIZE})
+----------------------------------\n").as_str())
+    }
+}
+
+fn read_problem(data: String) -> Result<(Params, Vec<Case>), Berror> {
+    let lines: Vec<&str> = data.split('\n').collect();
+    // println!("line {:?}", lines);
+    let header: Vec<&str> = lines[0].trim().split([' ', '\t']).collect();
+    let varnumber: i32 = header[0].parse()?;
+    let random_number: i32 = header[1].parse()?;
+    let min_random: f32 = header[2].parse()?;
+    let max_random: f32 = header[3].parse()?;
+    let num_cases: usize = header[4].parse()?;
+
+    let mut cases: Vec<Case> = Vec::with_capacity(num_cases);
+    for i in 0..num_cases {
+        let tokens: Vec<&str> = lines[i + 1]
+            .trim()
+            .split([' ', '\t'])
+            .filter(|t| !t.is_empty())
+            .collect();
+        let case: Case = tokens.iter().map(|t| t.parse().unwrap()).collect();
+        cases.push(case);
+    }
+
+    Ok((
+        Params {
+            seed: 5,
+            min_random,
+            max_random,
+            varnumber,
+            random_number,
+        },
+        cases,
+    ))
+}
+
+struct TinyGP {
+    params: Params,
+    cases: Vec<Case>,
+}
 
 impl TinyGP {
-    pub fn with_params() -> TinyGP {
-        todo!();
-    }
-
-    fn read_problem(data: String) -> Result<(Params, Vec<Case>), Berror> {
-        let lines: Vec<&str> = data.split('\n').collect();
-        // println!("line {:?}", lines);
-        let header: Vec<&str> = lines[0].split([' ', '\t']).collect();
-        let varnumber: i32 = header[0].parse()?;
-        let random_number: i32 = header[1].parse()?;
-        let min_random: f32 = header[2].parse()?;
-        let max_random: f32 = header[3].parse()?;
-        let num_cases: usize = header[4].parse()?;
-
-        let mut cases: Vec<Case> = Vec::with_capacity(num_cases);
-        for i in 0..num_cases {
-            let tokens: Vec<&str> = lines[i + 1]
-                .split([' ', '\t'])
-                .filter(|t| !t.is_empty())
-                .collect();
-            let case: Case = tokens.iter().map(|t| t.parse().unwrap()).collect();
-            cases.push(case);
-        }
-
-        Ok((
-            Params {
-                seed: 5,
-                min_random,
-                max_random,
-                varnumber,
-                random_number
-            },
-            cases,
-        ))
-    }
-
     pub fn from_problem(filename: &str) -> Result<TinyGP, Berror> {
-        todo!();
+        let content = fs::read_to_string(filename)?;
+        println!("{content}");
+        let (params, cases) = read_problem(content)?;
+        Ok(TinyGP { params, cases })
+    }
+
+    pub fn evolve(&self, generations: usize) {
+        println!("-- TINY GP (Rust version) --\nGENERATIONS={generations}\n{}", self.params);
     }
 }
 
@@ -62,16 +94,17 @@ fn main() {
     let seed: Option<i32> = Some(3);
     let filename = "../linear.dat";
 
-    let tgp = TinyGP::from_problem(filename);
+    let tgp = TinyGP::from_problem(filename).unwrap();
+    tgp.evolve(100);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::TinyGP;
+    use super::*;
 
     #[test]
     fn test_read_params() {
-        let (param, cases) = match TinyGP::read_problem(
+        let (param, cases) = match read_problem(
             "1 100 -5 5 10
 0 1
 1	2
