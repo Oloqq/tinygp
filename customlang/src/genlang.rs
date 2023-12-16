114188ed435ba0fe19fdafebf4bb82d92ee7d3b1
Olgierd Piofczyk <olgierdpk@gmail.com>
2023-12-17 00:38:31 +0100

    tokenize

--- customlang/src/genlang.rs
+++ customlang/src/genlang.rs
@@ -1,8 +1,10 @@
 #![allow(unused)]
 
-use std::{str::FromStr, error::Error, fmt::format};
+use std::{error::Error, fmt::format, str::FromStr};
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, EnumIter)]
 enum Keyword {
     ADD,
 }
@@ -11,7 +13,7 @@ enum Keyword {
 enum Opcode {
     Kw(Keyword),
     Const(i32),
-    Reg(usize)
+    Reg(usize),
 }
 
 impl FromStr for Opcode {
@@ -22,26 +24,26 @@ impl FromStr for Opcode {
         if s.starts_with("R") {
             return match s[1..].parse::<usize>() {
                 Ok(num) => Ok(Opcode::Reg(num)),
-                Err(_) => Err(format!("invalid register: {s}"))
-            }
-        }
-        else if s.starts_with(|c: char| c.is_digit(10) || c == '-') {
+                Err(_) => Err(format!("invalid register: {s}")),
+            };
+        } else if s.starts_with(|c: char| c.is_digit(10) || c == '-') {
             return match s.parse::<i32>() {
                 Ok(num) => Ok(Opcode::Const(num)),
-                Err(_) => Err(format!("invalid constant: {s}"))
-            }
+                Err(_) => Err(format!("invalid constant: {s}")),
+            };
         }
         match s {
             "ADD" => Ok(Opcode::Kw(Keyword::ADD)),
-            _ => Err(String::from(format!("unknown keyword: {s}")))
+            _ => Err(String::from(format!("unknown keyword: {s}"))),
         }
     }
 }
 
-fn tokenize(program: &str) -> Vec<&str> {
+fn tokenize(program: &str) -> Vec<Opcode> {
     program
         .split([' ', '\t', '\r', '\n'])
         .filter(|t| t.len() > 0)
+        .map(|t| Opcode::from_str(t).unwrap())
         .collect()
 }
 
@@ -51,14 +53,17 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    #[ignore]
-    #[rustfmt::skip]
     fn test_tokenize() {
-        assert_eq!(tokenize("ADD 2 3"), ["ADD", "2", "3"]);      // happy path
-        assert_eq!(tokenize("  ADD  2 3  "), ["ADD", "2", "3"]); // multiple spaces
-        assert_eq!(tokenize("  ADD\t2 3  "), ["ADD", "2", "3"]); // tab
-        assert_eq!(tokenize("ADD\n  2 3"), ["ADD", "2", "3"]);   // unix newline
-        assert_eq!(tokenize("ADD\r\n  2 3"), ["ADD", "2", "3"]); // windows newline
+        let program = [
+            Opcode::Kw(Keyword::ADD),
+            Opcode::Const(2),
+            Opcode::Const(3),
+        ];
+        assert_eq!(tokenize("ADD 2 3"), program);
+        assert_eq!(tokenize("  ADD  2 3  "), program);
+        assert_eq!(tokenize("  ADD\t2 3  "), program);
+        assert_eq!(tokenize("ADD\n  2 3"), program);
+        assert_eq!(tokenize("ADD\r\n  2 3"), program);
     }
 
     #[test]
@@ -72,8 +77,26 @@ mod tests {
         assert_eq!(Opcode::from_str("0").unwrap(), Opcode::Const(0));
         assert_eq!(Opcode::from_str("999").unwrap(), Opcode::Const(999));
         assert_eq!(Opcode::from_str("-999").unwrap(), Opcode::Const(-999));
-        assert_eq!(Opcode::from_str("10.0").unwrap_err(), "invalid constant: 10.0");
-        assert_eq!(Opcode::from_str("1e2").unwrap_err(), "invalid constant: 1e2");
-
+        assert_eq!(
+            Opcode::from_str("10.0").unwrap_err(),
+            "invalid constant: 10.0"
+        );
+        assert_eq!(
+            Opcode::from_str("1e2").unwrap_err(),
+            "invalid constant: 1e2"
+        );
+        // keywords
+        assert_eq!(Opcode::from_str("ADD").unwrap(), Opcode::Kw(Keyword::ADD));
+        for kw in Keyword::iter() {
+            let s = format!("{:?}", kw);
+            assert_eq!(
+                Opcode::from_str(s.as_str()).expect(format!("you forgot to implement from_str for {:?}", kw).as_str()),
+                Opcode::Kw(kw)
+            );
+        }
+        assert_eq!(
+            Opcode::from_str("bruh").unwrap_err(),
+            "unknown keyword: bruh"
+        );
     }
 }
