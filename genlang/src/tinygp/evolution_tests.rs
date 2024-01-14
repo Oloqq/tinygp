c9262cd9fd4f3b01ed455e744e0fbfed7aa810f5
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-01-14 03:14:30 +0100

    11A, 11B

--- genlang/src/tinygp/evolution_tests.rs
+++ genlang/src/tinygp/evolution_tests.rs
@@ -38,7 +38,7 @@ fn test_e2e_identity() {
 }
 
 #[test]
-fn test_e2e_gen_1() {
+fn test_e2e_gen_1() { // 1.1.A
     let params = Params {
         seed: 0,
         memsize: 3,
@@ -59,6 +59,37 @@ fn test_e2e_gen_1() {
         (vec![0], vec![1]),
         (vec![1], vec![1]),
         (vec![1, 2], vec![1])
+    ];
+    let writer: Box<dyn Write> = Box::new(io::stdout());
+    let seed = Some(0);
+    let mut tgp = TinyGP::new(params, cases, seed, writer.into());
+    let (program, fitness) = tgp.evolve(3);
+    println!("{:?}", program);
+    println!("{:?}", fitness);
+    assert_eq!(fitness, 0.0);
+}
+
+#[test]
+fn test_e2e_gen_789() { // 1.1.B
+    let params = Params {
+        seed: 0,
+        memsize: 3,
+        popsize: 100,
+        max_depth: 3,
+        max_size: 4,
+        acceptable_error: 0.0,
+        growing: GrowingParams {
+            p_prefer_reg_over_num: 0.0,
+            min_const: 789,
+            max_const: 790,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+    let cases: Vec<Case> = vec![
+        (vec![], vec![789]),
+        (vec![], vec![789]),
+        (vec![], vec![789])
     ];
     let writer: Box<dyn Write> = Box::new(io::stdout());
     let seed = Some(0);
