e695bfd63503db28052f7b1354c29ecfee2012f3
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-01-30 07:35:06 +0100

    prep more benchmarks

--- genlang/src/benchmark/bench_1_3.rs
+++ genlang/src/benchmark/bench_1_3.rs
@@ -3,8 +3,9 @@ use crate::tinygp::fitness_funcs::*;
 use crate::Args;
 use super::util::execute_benchmark;
 
-// 1.1.A Program powinien wygenerować na wyjściu (na dowolnej pozycji w danych wyjściowych) liczbę 1. Poza liczbą 1 może też zwrócić inne liczby.
-pub fn bench_1_1_a(args: &Args) {
+// 1.2.A Program powinien odczytać dwie pierwsze liczy z wejścia i zwrócić na wyjściu (jedynie) ich sumę. Na wejściu mogą być tylko całkowite liczby dodatnie w zakresie [0,9]
+pub fn bench_1_3_a(args: &Args) {
+    todo!();
     let params = Params {
         memsize: 3,
         popsize: 100,
@@ -25,68 +26,9 @@ pub fn bench_1_1_a(args: &Args) {
         (vec![1, 2], vec![1]),
     ];
 
-    execute_benchmark(args, params, cases, "1_1_a", diff_first);
+    execute_benchmark(args, params, cases, "1_3_a", diff_first);
 }
 
-// 1.1.B Program powinien wygenerować na wyjściu (na dowolnej pozycji w danych wyjściowych) liczbę 789. Poza liczbą 789 może też zwrócić inne liczby.
-pub fn bench_1_1_b(args: &Args) {
-    let params = Params {
-        memsize: 3,
-        popsize: 100,
-        max_size: 4, // ignored during initial generation, low number prevents bloating
-        p_crossover: 0.9,
-        p_mut_per_node: 0.1,
-        tournament_size: 2,
-        growing: GrowingParams {
-            p_prefer_reg_over_num: 0.0,
-            max_const: 1000,
-            ..Default::default()
-        },
-        ..Default::default()
-    };
-    let cases: Vec<Case> = vec![
-        (vec![0], vec![789]),
-        (vec![1], vec![789]),
-        (vec![1, 2], vec![789]),
-    ];
-
-    execute_benchmark(args, params, cases, "1_1_b", diff_first);
-}
-
-// 1.1.C Program powinien wygenerować na wyjściu (na dowolnej pozycji w danych wyjściowych) liczbę 31415. Poza liczbą 31415 może też zwrócić inne liczby.
-pub fn bench_1_1_c(args: &Args) {
-    let params = Params {
-        memsize: 3,
-        popsize: 1000,
-        max_size: 4,
-        p_crossover: 0.5,
-        p_mut_per_node: 0.1,
-        tournament_size: 2,
-        growing: GrowingParams {
-            max_const: 35000,
-            ..Default::default()
-        },
-        ..Default::default()
-    };
-    let cases: Vec<Case> = vec![
-        (vec![0], vec![31415]),
-        (vec![1], vec![31415]),
-        (vec![1, 2], vec![31415]),
-    ];
-
-    execute_benchmark(args, params, cases, "1_1_c", diff_first);
-}
-
-// 1.1.D Program powinien wygenerować na pierwszej pozycji na wyjściu liczbę 1. Poza liczbą 1 może też zwrócić inne liczby.
-// rozwiazane przez 1.1.A
-
-// 1.1.E Program powinien wygenerować na pierwszej pozycji na wyjściu liczbę 789. Poza liczbą 789 może też zwrócić inne liczby.
-// rozwiazane przez 1.1.B
-
-// 1.1.F Program powinien wygenerować na wyjściu liczbę jako jedyną liczbę 1. Poza liczbą 1 NIE powinien nic więcej wygenerować.
-// rozwiazane przez 1.1.A
-
-// 1.2.A Program powinien odczytać dwie pierwsze liczy z wejścia i zwrócić na wyjściu (jedynie) ich sumę. Na wejściu mogą być tylko całkowite liczby dodatnie w zakresie [0,9]
 
 // 1.2.B Program powinien odczytać dwie pierwsze liczy z wejścia i zwrócić na wyjściu (jedynie) ich sumę. Na wejściu mogą być tylko całkowite liczby w zakresie [-9,9]
 
--- genlang/src/benchmark/mod.rs
+++ genlang/src/benchmark/mod.rs
@@ -2,20 +2,28 @@ use crate::Args;
 
 use self::bench_1_1::*;
 
-mod util;
 mod bench_1_1;
+mod bench_1_2;
+mod bench_1_3;
+mod bench_1_4;
+mod util;
 
 pub fn run_benchmark(suite: &str, args: &Args) {
     match suite {
-        "1_1_a" => {
-            bench_1_1_a(args);
-        },
-        "1_1_b" => {
-            bench_1_1_b(args);
-        },
-        "1_1_c" => {
-            bench_1_1_c(args);
-        },
+        "1_1_a" => bench_1_1_a(args),
+        "1_1_b" => bench_1_1_b(args),
+        "1_1_c" => bench_1_1_c(args),
+
+        // "1_2_a" => bench_1_2_a(args),
+        // "1_2_b" => bench_1_2_b(args),
+        // "1_2_c" => bench_1_2_c(args),
+        // "1_2_d" => bench_1_2_d(args),
+        // "1_2_e" => bench_1_2_e(args),
+
+        // "1_3_a" => bench_1_3_a(args),
+        // "1_3_b" => bench_1_3_b(args),
+        // "1_4_a" => bench_1_4_a(args),
+        // "1_4_b" => bench_1_4_b(args),
         _ => {
             println!("Could not find the benchmark");
         }
